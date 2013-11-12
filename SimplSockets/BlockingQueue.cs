31c02158f34f72783eda56cf6b63590cc0667df3
David Haney <david.a.haney@gmail.com>
2013-11-12 00:12:47 -0500

    Added project files

---
+++ SimplSockets/BlockingQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SimplSockets
+{
+    internal sealed class BlockingQueue<T>
+    {
+        Queue<T> _queue = null;
+        Semaphore _semaphore = new Semaphore(0, Int32.MaxValue);
+
+        public BlockingQueue()
+        {
+            new Queue<T>();
+        }
+
+        public BlockingQueue(int capacity)
+        {
+            _queue = new Queue<T>(capacity);
+        }
+
+        public void Enqueue(T item)
+        {
+            lock (_queue)
+            {
+                _queue.Enqueue(item);
+            }
+
+            _semaphore.Release();
+        }
+
+        public T Dequeue()
+        {
+            _semaphore.WaitOne();
+
+            lock (_queue)
+            {
+                return _queue.Dequeue();
+            }
+        }
+    }
+}
---
+++ SimplSockets/Pool.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimplSockets
+{
+    internal sealed class Pool<T> where T : class
+    {
+        private readonly Queue<T> _queue = null;
+        private readonly int _initialPoolCount = 0;
+        private readonly Func<T> _newItemMethod = null;
+        private readonly Action<T> _resetItemMethod = null;
+
+        public Pool(int poolCount, Func<T> newItemMethod, Action<T> resetItemMethod)
+        {
+            _queue = new Queue<T>(poolCount);
+            _initialPoolCount = poolCount;
+            _newItemMethod = newItemMethod;
+            _resetItemMethod = resetItemMethod;
+        }
+
+        public void Push(T item)
+        {
+            // Limit queue size
+            if (_queue.Count > _initialPoolCount)
+            {
+                return;
+            }
+
+            lock (_queue)
+            {
+                _queue.Enqueue(item);
+            }
+        }
+
+        public T Pop()
+        {
+            T result = null;
+
+            if (_queue.Count == 0)
+            {
+                result = _newItemMethod();
+                if (_resetItemMethod != null)
+                {
+                    _resetItemMethod(result);
+                }
+                return result;
+            }
+
+            lock (_queue)
+            {
+                if (_queue.Count == 0)
+                {
+                    return _newItemMethod();
+                }
+
+                result = _queue.Dequeue();
+                if (_resetItemMethod != null)
+                {
+                    _resetItemMethod(result);
+                }
+                return result;
+            }
+        }
+    }
+}
