4e046a649faa55f5fccf39a9adc876ac187899cf
David Haney <david.a.haney@gmail.com>
2013-11-12 00:14:47 -0500

    Cleaned up XML comment warnings

--- SimplSockets/SimplSocket.cs
+++ SimplSockets/SimplSocket.cs
@@ -740,10 +740,17 @@ namespace SimplSockets
             public int TotalBytesToRead = -1;
         }
 
+        /// <summary>
+        /// A received message.
+        /// </summary>
         public class ReceivedMessage
         {
             internal Socket Socket;
             internal int ThreadId;
+
+            /// <summary>
+            /// The message bytes.
+            /// </summary>
             public byte[] Message;
         }
 
