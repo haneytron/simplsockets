d070c55a2e13a9ca1304d7ebb97ac9a390aa6601
David Haney <david.a.haney@gmail.com>
2015-01-07 19:57:12 -0500

    Bumped year in AssemblyInfo

--- SimplSockets/Properties/AssemblyInfo.cs
+++ SimplSockets/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Imperative Bytes, LLC")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("SimplSockets")]
